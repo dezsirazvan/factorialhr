002f66bebcb89d061c65c3da447717a8881660bc
Dezsi Razvan <dezsirazvan@macbook-pro-de-paack.home>
2021-07-19 18:57:59 +0200

    Add papaertrail

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -21,8 +21,4 @@ export default {
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
 </style>
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
+  has_paper_trail
+
   validates :phone_number, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
