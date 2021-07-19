2ab6695a919afa4f7ba2155d79ea608267ddf778
Dezsi Razvan <dezsirazvan@macbook-pro-de-paack.home>
2021-07-19 13:45:40 +0200

    Add contact model

---
+++ app/models/contact.rb
@@ -0,0 +1,6 @@
+class Contact < ApplicationRecord
+  validates :phone_number, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+end
